31014365ee10b17fe566ae3b9b1fd2eaf20a67d9
Pastukh Mariia <mahka230@gmail.com>
2025-01-29 11:07:20 +0200

    page destroy setting

--- vite.config.js
+++ vite.config.js
@@ -16,18 +16,23 @@
 //   base: process.env.VITE_ENV === 'production' ? '/test-task/' : '/',
 // })
 
-
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://mariiapmm.github.io/test-task/',
   plugins: [vue()],
+  build: {
+    outDir: 'dist', // Вказуємо, щоб Vite будував проект у папку docs
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: import.meta.env.VITE_ENV === 'production' ? '/test-task/' : '/',
+  // base: env === 'production' ? '/test-task/' : '/', // Налаштування шляху залежно від середовища
 })
